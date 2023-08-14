2c5f573c585b411d0230004bf4dff6a86b113ff7
kassy <92108722+kausaratg@users.noreply.github.com>
2023-08-14 21:55:22 +0100

    created qr code

--- code_file/views.py
+++ code_file/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
-
+import qrcode
+from time import time
+from django.conf import settings
 # Create your views here.
+
+def qrcode_generator(request):
+    if request.method == 'POST':
+        mylink =request.POST['link']
+        qr = qrcode.make(mylink)
+        qr_name = f'{str(time())}.png'
+        qr.save(settings.MEDIA_ROOT +'/'+ qr_name)
+        context = {'qr_name':qr_name}
+        return render(request, 'index.html', context)
+    else:
+        return render(request, 'index.html')
